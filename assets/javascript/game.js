4ee7a2cbcc6d2dee30d7db653de93cb194ab5599
Chillyssa <awilliams62090@gmail.com>
2018-08-03 20:58:27 -0600

    fixing some Java and adding more style

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,38 +3,52 @@ var losses = 0;
 var guessesLeft = 9;
 var guessesSoFar = [];
 var randomLetter = null;
-var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+var errormessage = "The Psychic got you! The letter was ";
+var winmessage = "Amazing! You read my mind! The letter was ";
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
 var reset = function () {
     guessesLeft = 9;
     guessesSoFar = [];
+    initialize();
+    updateguessesLeft();
+    updateguessesSoFar();
+};
+
+var initialize = function() {
     randomLetter = letters[Math.floor(Math.random() * letters.length)];
-    updateguessesLeft()
-    updateguessesSoFar()
-}
+};
 var updateguessesLeft = function () {
-    document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft
-}
+    document.getElementById("guessesLeft").innerHTML = "Guesses Left: " + guessesLeft;
+};
 var updateguessesSoFar = function () {
-    document.getElementById("guessesSoFar").innerHTML = "Guesses So Far: " + guessesSoFar.join(", ")
-}
+    document.getElementById("guessesSoFar").innerHTML = "Guesses So Far: " + guessesSoFar.join(", ");
+};
 document.onkeyup = function (event) {
-    console.log(event)
-    var letterGuessed = event.key
-    guessesSoFar.push(letterGuessed)
-    guessesLeft--
-    updateguessesLeft()
-    updateguessesSoFar()
+    console.log(event);
+    initialize();
+    var letterGuessed = event.key;
+    guessesSoFar.push(letterGuessed);
+    guessesLeft--;
+    updateguessesLeft();
+    updateguessesSoFar();
     if (letterGuessed === randomLetter) {
-        alert ("Amazing! You read my mind!") + reset() + wins++;
-        $("#wins").text("Wins: " + wins)
+        var msg2 = winmessage.concat(randomLetter);
+        alert (msg2);
+        reset();
+        wins++;
+        $("#wins").text("Player: " + wins);
     } else if (letterGuessed !== randomLetter) {
         updateguessesLeft();
     }
     if (guessesLeft < 0) {
-        alert ("Sorry, you've lost! Play again!") + reset() + losses++;
-        $("#losses").text("Losses: " + losses)
+        var msg = errormessage.concat(randomLetter);
+        alert (msg);
+        reset();
+        losses++;
+        $("#losses").text("Psychic: " + losses);
     } else {
-        updateguessesLeft()
+        updateguessesLeft();
     }
    
 }
